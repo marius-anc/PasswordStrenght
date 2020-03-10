e52acf89362631c41376a46d9a69671d118b329c
Marius Iulian <marius.ancutescu@outlook.com>
2020-03-10 20:29:21 +0200

    .

--- PasswordStrenghtTests/ProgramTests.cs
+++ PasswordStrenghtTests/ProgramTests.cs
@@ -8,25 +8,38 @@ namespace PasswordStrenght.Tests
         [Fact]
         public void GetCorrectlyLowerCaseCount()
         {
-            Assert.True(Program.GetLowerCaseCount("abcdeAB12+-") >= 5);
+            Assert.True(Program.GetLowerCaseCount("abcdeAB12+-") == 5);
         }
 
         [Fact]
         public void GetCorrectlyUpperCaseCount()
         {
-            Assert.True(Program.GetUpperCaseCount("abcdeAB12+-") >= 2);
+            Assert.True(Program.GetUpperCaseCount("abcdeAB12+-") == 2);
         }
 
         [Fact]
         public void GetCorrectlyDigitsCount()
         {
-            Assert.True(Program.GetDigitsCount("abcdeAB12+-") >= 2);
+            Assert.True(Program.GetDigitsCount("abcdeAB12+-") == 2);
         }
 
         [Fact]
         public void GetCorrectlySymbolCount()
         {
-            Assert.True(Program.GetSymbolCount("abcdeAB12+-") >= 2);
+            Assert.True(Program.GetSymbolCount("abcdeAB12+-") == 2);
+        }
+
+        [Fact]
+        public void ChecksForSimilarChars()
+        {
+            Assert.True(Program.ContainsSimilarChars("ABCDabcdI"));
+        }
+
+
+        [Fact]
+        public void ChecksForAmbigousChars()
+        {
+            Assert.True(Program.ContainsSimilarChars("ABCDabcdI}"));
         }
     }
 }
